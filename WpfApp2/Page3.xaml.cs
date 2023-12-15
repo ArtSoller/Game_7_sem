a715b470ccbd94c9cd06b18fa7cafc1db655395b
Егор Янковский <yankovskij.2019@azure.nstu.ru>
2023-12-16 00:54:34 +0700

    Cleaned some things

--- WpfApp2/Page3.xaml.cs
+++ WpfApp2/Page3.xaml.cs
@@ -1,112 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
 
-namespace WpfApp2
+namespace WpfApp2;
+
+/// <summary>
+/// Логика взаимодействия для Page3.xaml
+/// </summary>
+public partial class Page3
 {
-    /// <summary>
-    /// Логика взаимодействия для Page3.xaml
-    /// </summary>
-    public partial class Page3 : Page
+    private bool isDragging = false;
+    private Point startPoint;
+
+    public Page3()
     {
-        public Page3()
+        InitializeComponent();
+    }
+
+    //private void CanvasKeyDown(object sender, KeyEventArgs e)
+    //{
+    //    if (e.Key == Key.F)
+    //    {
+    //        // Открываем страницу для прямоугольника 1
+    //        NavigationService.Navigate(new Page1());
+    //    }
+    //}
+
+    private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
         {
-            InitializeComponent();
+            startPoint = e.GetPosition(null);
+            isDragging = true;
+            ((Image)sender).CaptureMouse();
         }
+    }
 
-        private void CanvasKeyDown(object sender, KeyEventArgs e)
+    private void Image_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (isDragging)
         {
-            if (e.Key == Key.F)
+            Point position = e.GetPosition(null);
+            var tt = new TranslateTransform
             {
-                // Открываем страницу для прямоугольника 1
-                NavigationService.Navigate(new Page1());
-
-            }
+                X = position.X - startPoint.X,
+                Y = position.Y - startPoint.Y
+            };
+            ((Image)sender).RenderTransform = tt;
         }
+    }
 
-        private bool isDragging = false;
-        private Point startPoint;
-
-        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+    private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+        if (isDragging)
         {
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                startPoint = e.GetPosition(null);
-                isDragging = true;
-                ((Image)sender).CaptureMouse();
-            }
+            isDragging = false;
+            ((Image)sender).ReleaseMouseCapture();
         }
+    }
 
-        private void Image_MouseMove(object sender, MouseEventArgs e)
+    private void Target_DragEnter(object sender, DragEventArgs e)
+    {
+        if (e.Data.GetDataPresent(DataFormats.FileDrop))
         {
-            if (isDragging)
-            {
-                Point position = e.GetPosition(null);
-                TranslateTransform tt = new TranslateTransform();
-                tt.X = position.X - startPoint.X;
-                tt.Y = position.Y - startPoint.Y;
-                ((Image)sender).RenderTransform = tt;
-            }
+            e.Effects = DragDropEffects.Copy;
         }
-
-        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        else
         {
-            if (isDragging)
-            {
-                isDragging = false;
-                ((Image)sender).ReleaseMouseCapture();
-            }
+            e.Effects = DragDropEffects.None;
         }
+    }
 
-        /// <summary>
-        /// 
-        ///
-        private void Target_DragEnter(object sender, DragEventArgs e)
+    private void Target_Drop(object sender, DragEventArgs e)
+    {
+        if (e.Data.GetDataPresent(DataFormats.FileDrop))
         {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var file in files)
             {
-                e.Effects = DragDropEffects.Copy;
-            }
-            else
-            {
-                e.Effects = DragDropEffects.None;
-            }
-        }
 
-        private void Target_Drop(object sender, DragEventArgs e)
-        {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
-            {
-                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                foreach (string file in files)
+                // Создать новый Image элемент и добавить его в вашу страницу
+                var newImage = new Image
                 {
-                    // Создать новый Image элемент и добавить его в вашу страницу
-                    Image newImage = new Image();
-                    newImage.Source = new BitmapImage(new Uri(file));
-                    // Добавить новый Image элемент в вашу разметку
-                    // Например, добавить его в StackPanel или другой контейнер
-                    ImageContainer.Children.Add(newImage);
+                    Source = new BitmapImage(new Uri(file))
+                };
 
-                    Border dropTarget = new Border();
-                    dropTarget.Background = Brushes.White;
-                    dropTarget.AllowDrop = true;
-                    dropTarget.DragEnter += Target_DragEnter;
-                    dropTarget.Drop += Target_Drop;
-                    DropTargets.Children.Add(dropTarget);
-                }
+                // Добавить новый Image элемент в вашу разметку
+                // Например, добавить его в StackPanel или другой контейнер
+                ImageContainer.Children.Add(newImage);
+
+                var dropTarget = new Border
+                {
+                    Background = Brushes.White,
+                    AllowDrop = true
+                };
+                dropTarget.DragEnter += Target_DragEnter;
+                dropTarget.Drop += Target_Drop;
+                DropTargets.Children.Add(dropTarget);
             }
         }
     }
