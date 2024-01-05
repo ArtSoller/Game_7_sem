4280dc29e6df016b686e7c2c9821eaa5a94ad89e
Егор Янковский <yankovskij.2019@azure.nstu.ru>
2024-01-05 20:30:48 +0700

    Музыка + логика квеста

--- WpfApp2/App.xaml.cs
+++ WpfApp2/App.xaml.cs
@@ -37,7 +37,7 @@ public partial class App : Application
         // "pack://siteoforigin:,,,/snd/backgroundMusic.wav"
         _openFileDialog = new()
         {
-            FileName = "D:\\CodeRepos\\CS\\NewGame\\Game_7_sem\\WpfApp2\\snd\\backgroundMusic.mp3"
+            FileName = "D:\\CodeRepos\\CS\\NewGame\\Game_7_sem\\WpfApp2\\snd\\Music1.mp3"
         };
         _mediaPlayer.MediaOpened += RunMusic;
         _mediaPlayer.MediaFailed += FailedMusic;
--- WpfApp2/obj/Debug/net8.0-windows/WpfApp2.AssemblyInfo.cs
+++ WpfApp2/obj/Debug/net8.0-windows/WpfApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02352e9f4a0f79ba8c4d35ea71496981fbddbb2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
